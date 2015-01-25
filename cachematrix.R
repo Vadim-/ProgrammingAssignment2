de40a8fbd75f6b9d24d0c8b6a5003bae3778ffbf
Vadim Soloviev <solovievadim@gmail.com>
2015-01-26 10:30:53 +1100

    This is a final submission for the exercise
    Functions bellow allow to compute the inverse of a matrix. Inverting a
    matrix is a costly operation, so it's beneficial to cache the result. We
    are assuming that a matrix is invertible

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Functions bellow allow to compute the inverse of a matrix
+# Inverting a matrix is a costly operation, so it's beneficial to cache the result
+# We are assuming that a matrix is invertible
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# This function creates a list object that enables us set and get a value of a matrix
+# and set and get a value of inverse of a matrix
 
+makeCacheMatrix <- function(matrix = matrix()) {
+  inverse_matrix <- NULL
+  set <- function(x) {
+    matrix <<- x
+    inverse_matrix <<- NULL
+  }
+  get <- function() matrix
+  set_inverse <- function(inverse) inverse_matrix <<- inverse
+  get_inverse <- function() inverse_matrix
+  return(list(set = set, get = get, set_inverse=set_inverse, get_inverse=get_inverse))
 }
 
+# This function returns the inverse of a matrix. First of all, the function checks
+# is the inverse has been computed before (and the matrix has not changed), then
+# the function gets the previous result and skips the computation. Otherwise, the
+# function computes the inverse and writes the value to cache
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(matrix, ...) {
+  inverse_matrix <- matrix$get_inverse()
+  if(!is.null(inverse_matrix)) {
+    message("Getting cached data...")
+    return(inverse_matrix)
+  }
+  data <- matrix$get()
+  inverse_matrix <- solve(data, ...)
+  matrix$set_inverse(inverse_matrix)
+  return(inverse_matrix)
 }
